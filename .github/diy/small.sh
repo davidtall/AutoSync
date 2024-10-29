a6d854ca0c6a895ce8dd154a792b7b4b6390b9c7
davidtall <269700927@qq.com>
2024-10-29 15:07:02 +0800

    add daed

--- .github/diy/small.sh
+++ .github/diy/small.sh
@@ -32,6 +32,7 @@ git clone --depth 1 https://github.com/muink/luci-app-homeproxy
 
 #add ddns-go
 git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go ddnsgo && mv -n ddnsgo/luci-app-ddns-go ./; rm -rf ddnsgo
+git clone --depth 1 https://github.com/QiuSimons/luci-app-daed daed && mv -n daed/{luci-app-ddns-go,daed} ./; rm -rf daed
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
